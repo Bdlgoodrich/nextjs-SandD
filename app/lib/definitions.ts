b81f74941053cbf3991f679a5ef1b3d897f13f2b
Brianna Goodrich <bdlgoodrich@gmail.com>
2024-09-11 21:03:37 -0600

    Added skill, drill, class definitions

--- app/lib/definitions.ts
+++ app/lib/definitions.ts
@@ -12,8 +12,6 @@ export type User = {
 export type Customer = {
   id: string;
   name: string;
-  email: string;
-  image_url: string;
 };
 
 export type Invoice = {
@@ -21,15 +19,8 @@ export type Invoice = {
   customer_id: string;
   amount: number;
   date: string;
-  // In TypeScript, this is called a string union type.
-  // It means that the "status" property can only be one of the two strings: 'pending' or 'paid'.
-  status: 'pending' | 'paid';
 };
 
-export type Revenue = {
-  month: string;
-  revenue: number;
-};
 
 export type LatestInvoice = {
   id: string;
